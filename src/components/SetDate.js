f7d2d7a676fe5e41e24ceff07621bb99777119d8
itscoolvikash@gmail.com <itscoolvikash@gmail.com>
2024-01-25 23:04:08 +0530

    2nd commit

--- src/components/SetDate.js
+++ src/components/SetDate.js
@@ -1,19 +1,37 @@
 import React, { useState } from 'react'
-import { DateRangePicker } from 'rsuite';
-// import { Link } from 'react-router-dom';
+import { DateRangePicker } from "react-date-range";
 import { Button } from 'react-bootstrap';
-// import {useState}  from 'react';
 import 'rsuite/dist/rsuite.min.css';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import "../App.css"
 import { Link } from 'react-router-dom';
+import { addDays } from 'date-fns';
+
+
+
+
 const SetDate = () => {
-    const [date,setDate]=useState()
+    const [state, setState] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection'
+        }
+      ]);
+      console.log(state)
   return (
     <>
     <div className='dates'>
       <span><strong>Set Your Date Range Here</strong><Link to='/' className='close'><button type="button" class="btn btn-danger">close</button></Link></span>   
-      <DateRangePicker className='date'/>
-      
+     
+      <DateRangePicker
+      onChange={item => setState([item.selection])}
+      showSelectionPreview={true}
+      moveRangeOnFirstSelection={false}
+      months={2}
+      ranges={state}
+     direction="horizontal"/>
     </div>
    
     </>
